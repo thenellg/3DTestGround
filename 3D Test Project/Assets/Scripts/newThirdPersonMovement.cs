de4e92b1fb10386b591b8329200ad3ed5c3d30c3
thenellg <thenellg@oregonstate.edu>
2020-09-06 22:14:06 -0700

    Fixed some stuff. See dev logs on 9/6/2020 in Wiki.

--- 3D Test Project/Assets/Scripts/newThirdPersonMovement.cs
+++ 3D Test Project/Assets/Scripts/newThirdPersonMovement.cs
@@ -11,18 +11,19 @@ public class newThirdPersonMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     [SerializeField] private Vector3 inputVector;
+    public Animator playerAnim;
 
     //jumping
     [SerializeField] bool moving;
     public float gravity = -9.81f;
     public float jumpHeight = 3;
     float jumpForce;
-    float jump2Timer = 2;
+    [SerializeField] float jump2Timer = 2;
     [SerializeField] bool jump2 = false;
-    float jump3Timer = 2;
+    [SerializeField] float jump3Timer = 2;
     [SerializeField] bool jump3 = false;
     bool jumpCheck = true;
-    public float secondJumpPoint = 0.4f;
+    public float secondJumpPoint = 3f; //0.4f
 
     //gravity
     public float vertical = -10f;
@@ -64,6 +65,27 @@ public class newThirdPersonMovement : MonoBehaviour
             gravity = 0f;
         }
 
+        //rb.AddForce(0, gravity, 0, ForceMode.VelocityChange);
+
+
+        //Implementing a sprint function
+        if (Input.GetButtonDown("Sprint"))
+        {
+            if (sprinting)
+            {
+                sprinting = false;
+            }
+            else
+            {
+                sprinting = true;
+            }
+        }
+
+        if (sprinting)
+        {
+            trueSpeed = speed * 1.15f;
+        }
+
         //setting movement variable for triple jump
         if ((new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized).magnitude > 0.1f)
             moving = true;
@@ -88,28 +110,30 @@ public class newThirdPersonMovement : MonoBehaviour
         }
 
         //The actual jump
+
         jumpForce = gravity;
         if (Input.GetButtonDown("Jump") && isGrounded && jumpCheck)
         {
+            playerAnim.SetTrigger("playerJumps");
             if (jump2 && moving)
             {
                 jump3 = true;
-                jump3Timer = secondJumpPoint;
-                jumpForce = Mathf.Sqrt((jumpHeight * 1.25f) * -2 * tempGrav);
+                jump3Timer = 2f;
+                jumpForce = jumpHeight * 1.2f;
                 //playerAnim.SetTrigger("DoubleJump");
                 jump2 = false;
             }
             else if (jump3 && moving)
             {
-                jumpForce = Mathf.Sqrt((jumpHeight * 2) * -2 * tempGrav);
+                jumpForce = jumpHeight * 1.45f;
                 //playerAnim.SetTrigger("TripleJump");
                 jump3 = false;
             }
             else
             {
-                jumpForce = Mathf.Sqrt(jumpHeight * -2 * tempGrav);
+                jumpForce = jumpHeight;
                 //playerAnim.SetTrigger("SingleJump");
-                jump2Timer = secondJumpPoint;
+                jump2Timer = 2f;
                 jump2 = true;
             }
 
@@ -128,27 +152,8 @@ public class newThirdPersonMovement : MonoBehaviour
         }
         */
 
-        //Implementing a sprint function
-        if (Input.GetButtonDown("Sprint"))
-        {
-            if (sprinting)
-            {
-                sprinting = false;
-            }
-            else
-            {
-                sprinting = true;
-            }
-        }
-
-        if (sprinting)
-        {
-            trueSpeed = speed * 1.5f;
-        }
-
         //implementing gravity
         //vertical = rb.velocity.y;                              //Uncomment this line for ramping
-        //rb.AddForce(0f, Physics.gravity.y, 0f);
         rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
 
         //Determining character movement
@@ -157,7 +162,7 @@ public class newThirdPersonMovement : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         //Moving character
-        
+
 
         if (moving)
         {
@@ -170,15 +175,13 @@ public class newThirdPersonMovement : MonoBehaviour
 
             inputVector = inputVector.normalized * trueSpeed;
             //rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-            rb.AddForce(inputVector.x, inputVector.y, inputVector.z);
+            rb.AddForce(inputVector.x * trueSpeed, inputVector.y * trueSpeed, inputVector.z * trueSpeed);
         }
-        /*
         else
         {
-            rb.velocity = new Vector3(0f,rb.velocity.y, 0f);
+            //rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
             transform.rotation = tempRotation;
             sprinting = false;
         }
-        */
     }
 }
